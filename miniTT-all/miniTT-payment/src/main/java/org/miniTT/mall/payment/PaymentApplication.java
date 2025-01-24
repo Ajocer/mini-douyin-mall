5027a511d2f2655bc0ea36264df5a21a0b85be60
Ajocer <zaixiajumou@gmail.com>
2025-01-24 21:36:58 +0800

    feat(service): 添加服务发现和 MyBatis 扫描
    - 在 CheckoutApplication、GatewayApplication、PaymentApplication 和 ProductApplication 中添加 @EnableDiscoveryClient 注解，启用服务发现功能
    - 在 CheckoutApplication、PaymentApplication 和 ProductApplication 中添加 @MapperScan 注解，指定 MyBatis映射器扫描包

--- miniTT-all/miniTT-payment/src/main/java/org/miniTT/mall/payment/PaymentApplication.java
+++ miniTT-all/miniTT-payment/src/main/java/org/miniTT/mall/payment/PaymentApplication.java
@@ -1,9 +1,13 @@
 package org.miniTT.mall.payment;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("org.miniTT.mall.payment.dao.mapper")
 public class PaymentApplication {
     public static void main(String[] args) {
         SpringApplication.run(PaymentApplication.class, args);
