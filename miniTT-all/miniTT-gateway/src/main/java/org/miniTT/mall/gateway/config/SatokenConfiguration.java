a4dc07ab71c67b79672bd555fbf810559afab5ef
汉尼拔今天吃什么 <3495746536@qq.com>
2025-01-26 20:26:42 +0800

    fix(gateway): 解决spring-boot-starter-web和spring cloud gateway的冲突

--- miniTT-all/miniTT-gateway/src/main/java/org/miniTT/mall/gateway/config/SatokenConfiguration.java
+++ miniTT-all/miniTT-gateway/src/main/java/org/miniTT/mall/gateway/config/SatokenConfiguration.java
@@ -1,30 +1,30 @@
-package org.miniTT.mall.gateway.config;
-
-import cn.dev33.satoken.interceptor.SaInterceptor;
-import cn.dev33.satoken.router.SaRouter;
-import cn.dev33.satoken.stp.StpUtil;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
-import java.util.List;
-@Configuration
-public class SatokenConfiguration implements WebMvcConfigurer {
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new SaInterceptor(handle -> {
-            SaRouter
-                    .match("/**")
-                    .notMatch(excludePaths())
-                    .check(r -> StpUtil.checkLogin());
-        })).addPathPatterns("/**");
-    }
-
-    // 动态获取哪些 path 可以忽略鉴权
-    public List<String> excludePaths() {
-        // 此处仅为示例，实际项目你可以写任意代码来查询这些path
-        return Arrays.asList("/path1", "/path2", "/api/miniTT/user/v1/register","/api/miniTT/user/v1/login");
-    }
-
-}
+//package org.miniTT.mall.gateway.config;
+//
+//import cn.dev33.satoken.interceptor.SaInterceptor;
+//import cn.dev33.satoken.router.SaRouter;
+//import cn.dev33.satoken.stp.StpUtil;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import java.util.Arrays;
+//import java.util.List;
+//@Configuration
+//public class SatokenConfiguration implements WebMvcConfigurer {
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new SaInterceptor(handle -> {
+//            SaRouter
+//                    .match("/**")
+//                    .notMatch(excludePaths())
+//                    .check(r -> StpUtil.checkLogin());
+//        })).addPathPatterns("/**");
+//    }
+//
+//    // 动态获取哪些 path 可以忽略鉴权
+//    public List<String> excludePaths() {
+//        // 此处仅为示例，实际项目你可以写任意代码来查询这些path
+//        return Arrays.asList("/path1", "/path2", "/api/miniTT/user/v1/register","/api/miniTT/user/v1/login");
+//    }
+//
+//}
